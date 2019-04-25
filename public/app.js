4917266e3c92a8f7f30bee8ca7724ac24aa03bb9
Justin Smith <justinsmith@Justins-MacBook-Pro.local>
2019-04-25 16:57:28 -0400

    connected to pokemon api

--- public/app.js
+++ public/app.js
@@ -1,1 +1,20 @@
-console.log('hi');
+const app = angular.module('MyApp', []);
+
+app.controller('MainController', ['$http', function($http){
+  this.appName = "Pokemon Fun Time!"
+  this.pokemonURL = "https://pokeapi.co/api/v2/pokemon/?offset=20&limit=200"
+  this.getPokemon = () => {
+    $http({
+      method: 'GET',
+      url: this.pokemonURL
+    }).then(response =>{
+        console.log(response.data);
+    }, error => {
+      console.log(error);
+    })
+  };
+
+
+
+
+}]);
