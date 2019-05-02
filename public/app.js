c92ccb691a7e971e4c3a83381a4c60ae2543c7a1
Justin Smith <justinsmith@Justins-MacBook-Pro.local>
2019-05-02 17:09:32 -0400

    fixed some ul issues and began styling

--- public/app.js
+++ public/app.js
@@ -13,7 +13,7 @@ app.controller('MainController', ['$http', function($http){
   this.pokemonAblilites = [];
   this.myCards = [];
   this.varDup = ''
-  this.pokemonURL = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=30"
+  this.pokemonURL = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=964"
   this.baseUrl = "https://pokeapi.co/api/v2/pokemon/"
 
   this.findPokemon = ()=>{
@@ -59,10 +59,12 @@ app.controller('MainController', ['$http', function($http){
 
   this.buyPokemon = () => {
     let newCard = angular.element(document.getElementsByClassName("myCards"));
-    if (this.pokemonAblilites[2]) {
-        newCard.append('<div class="myCard"><h1>'+this.pokemonName+'</h1><img src="'+this.pokemonSprites+'" alt="'+this.pokemonName+'"/><h3>Abilities</h3><ul class="myAbilities"><li>'+this.pokemonAblilites[0].ability.name+'</li><li>'+this.pokemonAblilites[1].ability.name+'</li><li>'+this.pokemonAblilites[2].ability.name+'</li></ul><h3>Stats</h3><ul class="myStats"><li>'+this.pokemonStats[0].stat.name+': '+this.pokemonStats[0].base_stat+'</li><li>'+this.pokemonStats[1].stat.name+': '+this.pokemonStats[1].base_stat+'</li><li>'+this.pokemonStats[2].stat.name+': '+this.pokemonStats[2].base_stat+'</li><li>'+this.pokemonStats[3].stat.name+': '+this.pokemonStats[3].base_stat+'</li><li>'+this.pokemonStats[4].stat.name+': '+this.pokemonStats[4].base_stat+'</li><li>'+this.pokemonStats[5].stat.name+': '+this.pokemonStats[5].base_stat+'</li></ul></div>');
-      }else{
+    if (this.pokemonAblilites[2]){
+      newCard.append('<div class="myCard"><h1>'+this.pokemonName+'</h1><img src="'+this.pokemonSprites+'" alt="'+this.pokemonName+'"/><h3>Abilities</h3><ul class="myAbilities"><li>'+this.pokemonAblilites[0].ability.name+'</li><li>'+this.pokemonAblilites[1].ability.name+'</li><li>'+this.pokemonAblilites[2].ability.name+'</li></ul><h3>Stats</h3><ul class="myStats"><li>'+this.pokemonStats[0].stat.name+': '+this.pokemonStats[0].base_stat+'</li><li>'+this.pokemonStats[1].stat.name+': '+this.pokemonStats[1].base_stat+'</li><li>'+this.pokemonStats[2].stat.name+': '+this.pokemonStats[2].base_stat+'</li><li>'+this.pokemonStats[3].stat.name+': '+this.pokemonStats[3].base_stat+'</li><li>'+this.pokemonStats[4].stat.name+': '+this.pokemonStats[4].base_stat+'</li><li>'+this.pokemonStats[5].stat.name+': '+this.pokemonStats[5].base_stat+'</li></ul></div>');
+    } else if (this.pokemonAblilites[1]) {
         newCard.append('<div class="myCard"><h1>'+this.pokemonName+'</h1><img src="'+this.pokemonSprites+'" alt="'+this.pokemonName+'"/><h3>Abilities</h3><ul class="myAbilities"><li>'+this.pokemonAblilites[0].ability.name+'</li><li>'+this.pokemonAblilites[1].ability.name+'</li></ul><h3>Stats</h3><ul class="myStats"><li>'+this.pokemonStats[0].stat.name+': '+this.pokemonStats[0].base_stat+'</li><li>'+this.pokemonStats[1].stat.name+': '+this.pokemonStats[1].base_stat+'</li><li>'+this.pokemonStats[2].stat.name+': '+this.pokemonStats[2].base_stat+'</li><li>'+this.pokemonStats[3].stat.name+': '+this.pokemonStats[3].base_stat+'</li><li>'+this.pokemonStats[4].stat.name+': '+this.pokemonStats[4].base_stat+'</li><li>'+this.pokemonStats[5].stat.name+': '+this.pokemonStats[5].base_stat+'</li></ul></div>');
+      }else {
+        newCard.append('<div class="myCard"><h1>'+this.pokemonName+'</h1><img src="'+this.pokemonSprites+'" alt="'+this.pokemonName+'"/><h3>Abilities</h3><ul class="myAbilities"><li>'+this.pokemonAblilites[0].ability.name+'</li></ul><h3>Stats</h3><ul class="myStats"><li>'+this.pokemonStats[0].stat.name+': '+this.pokemonStats[0].base_stat+'</li><li>'+this.pokemonStats[1].stat.name+': '+this.pokemonStats[1].base_stat+'</li><li>'+this.pokemonStats[2].stat.name+': '+this.pokemonStats[2].base_stat+'</li><li>'+this.pokemonStats[3].stat.name+': '+this.pokemonStats[3].base_stat+'</li><li>'+this.pokemonStats[4].stat.name+': '+this.pokemonStats[4].base_stat+'</li><li>'+this.pokemonStats[5].stat.name+': '+this.pokemonStats[5].base_stat+'</li></ul></div>');
       };
     };
 
